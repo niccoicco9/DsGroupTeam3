547a651d0a70ed7ccbcdf82bbc25bcb7e3a3c719
Amila Fernando <amila_fernandp92@gmail.com>
2018-02-19 17:42:10 +0100

    creazione generatoreMeteo.js

---
+++ JavaScript/generatoreMeteo.js
@@ -0,0 +1,57 @@
+var Coordinate = {
+    lat: 145.77,
+    lon: -16.92
+}
+
+var Sys = {
+    country: "IT",
+    sunrise: 1369769524,
+    sunset: 1369821049,
+    type: 1,
+    id: 8166,
+    message: 0.0166,
+}
+
+var Weather = {
+    id: 805,
+    main: "Clouds",
+    description: "broken clouds",
+    icon: "04n"
+}
+
+var Main = {
+    temp: 289.5,
+    humidity: 89,
+    pressure: 1013,
+    temp_min: 287.04,
+    temp_max: 292.04
+}
+
+var Wind = {
+    speed: 5.1,
+    deg: 150
+}
+
+var Rain = {
+    "3h": 0
+}
+
+var Clouds = {
+    all: 75
+}
+
+var WeatherMain = {
+    coordinate: Coordinate,
+    sys: Sys,
+    weather: Weather,
+    main: Main,
+    wind: Wind,
+    rain: Rain,
+    clouds: Clouds,
+    dt: 1369824698,
+    id: 1851632,
+    name: "Milano",
+    base: "cmc stations",
+    cod: 200
+}
+
