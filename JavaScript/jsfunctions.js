6f92ba0b66a94bc0bf891926112f3222cd3a9015
Amila Fernando <amila_fernandp92@gmail.com>
2018-02-19 16:56:07 +0100

    modifica jsfuctions.js

---
+++ JavaScript/jsfunctions.js
@@ -0,0 +1,6 @@
+function caricaDati (nome, data) {
+    var pnomeapp = document.getElementById ("nome");
+    pnomeapp.innerText = nome;
+    var pdataapp = document.getElementById ("data");
+    pdataapp.innerText = data;
+}
