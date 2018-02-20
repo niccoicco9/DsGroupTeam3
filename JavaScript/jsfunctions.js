98c3310d29fd7f431d19df9394d5da82ca8bc8bc
Amila Fernando <amila_fernandp92@gmail.com>
2018-02-20 15:06:02 +0100

    modifiche js

--- JavaScript/jsfunctions.js
+++ JavaScript/jsfunctions.js
@@ -1,6 +1,10 @@
 function caricaDati (nome, data, arr) {
     addName (nome, data);
-    /*location;*/
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition (functionGo, functionError);
+    } else {
+        document.getElementById ("idgeocoor").innerText = "La geolocation non è disponibile";
+    }
     addTable (arr);
 }
 
@@ -9,31 +13,22 @@ function addName (nome, data) {
     document.getElementById ("data").innerText = data;
 }
 
-/*function location () {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition (funzioneOk, funzioneErrore);
-    } else {
-        alert('La geolocation non è disponibile');
-    }
-}*/
-
-function funzioneOk(posizione){
-    if(posizione && posizione.coords){
-        var latitudine = posizione.coords.latitude;
-        var longitudine  = posizione.coords.longitude;
-        var testo = "Latitudine: " + latitudine + " " + "Longitudine: " + longitudine;
-        alert (testo);
+function functionGo (posizione){
+    if(posizione && posizione.coords) {
+        var latitudine = posizione.coords.latitude.toFixed(2);
+        var longitudine  = posizione.coords.longitude.toFixed(2);
+        var app = "[" + latitudine + "," + longitudine + "]";
+        document.getElementById ("idgeocoor").innerText = app;
     }
 }
 
-function funzioneErrore(error) {
-    alert(error.message);
+function functionError (error) {
+    document.getElementById ("idgeocoor").innerText = error.message;
 }
 
 function addTable (arr) {
     document.getElementById ("idwind").innerText = arr[0];
     document.getElementById ("iddescription").innerText = arr[1];
-    document.getElementById ("idgeocoor").innerText = arr[2];
     document.getElementById ("idpressure").innerText = arr[3];
     document.getElementById ("idtemperature").innerText = arr[4];
     document.getElementById ("idsunset").innerText = arr[5];
