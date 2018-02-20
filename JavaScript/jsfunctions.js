29869465c97d2846ee74c5d66d5d34779cdde721
Amila Fernando <amila_fernandp92@gmail.com>
2018-02-20 10:21:26 +0100

    modifica jsfunctions.js

--- JavaScript/jsfunctions.js
+++ JavaScript/jsfunctions.js
@@ -3,4 +3,24 @@ function caricaDati (nome, data) {
     pnomeapp.innerText = nome;
     var pdataapp = document.getElementById ("data");
     pdataapp.innerText = data;
+    function location () {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition (funzioneOk, funzioneErrore);
+        } else {
+            alert('La geolocation non è disponibile');
+        }
+    }
+}
+
+function funzioneOk(posizione){
+    if(posizione && posizione.coords){
+        var latitudine = posizione.coords.latitude;
+        var longitudine  = posizione.coords.longitude;
+        var testo = "Latitudine: " + latitudine + " " + "Longitudine: " + longitudine;
+        alert (testo);
+    }
+}
+
+function funzioneErrore(error) {
+    alert(error.message);
 }
