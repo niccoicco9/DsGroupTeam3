5214ac41fc4cae311ec599f42103f5c816f4a342
Amila Fernando <amila_fernandp92@gmail.com>
2018-02-20 12:13:14 +0100

    modifica generatoreMeteo.js

--- JavaScript/generatoreMeteo.js
+++ JavaScript/generatoreMeteo.js
@@ -1,57 +1,47 @@
-var Coordinate = {
-    lat: 145.77,
-    lon: -16.92
+var weather = {
+    "coord": {
+        "lon": 45.77,
+        "lat": 9.92
+    },
+    "weather": [{
+        "id": 803,
+        "main": "Clouds",
+        "description": "Broken clouds",
+        "icon": "04n"
+    }],
+    "base": "cmc stations",
+    "main": {
+        "temp": 6,
+        "pressure": 1019,
+        "humidity": 83,
+        "temp_min": -1.5,
+        "temp_max": 7.2,
+        "sea_level": 10,
+        "grnd_level": 20
+    },
+    "wind":{
+        "speed": 5.1,
+        "deg": 150
+    },
+    "clouds": {
+        "all": 75
+    },
+    "rain": {
+        "3h": 3
+    },
+    "snow": {
+        "3h": 3
+    },
+    "dt": 1435658272,
+    "sys": {
+        "type": 1,
+        "id": 8166,
+        "message": 0.0166,
+        "country": "AU",
+        "sunrise": 1435610796,
+        "sunset": 1435650870
+    },
+    "id": 2172797,
+    "name": "Cairns",
+    "cod": 200
 }
-
-var Sys = {
-    country: "IT",
-    sunrise: 1369769524,
-    sunset: 1369821049,
-    type: 1,
-    id: 8166,
-    message: 0.0166,
-}
-
-var Weather = {
-    id: 805,
-    main: "Clouds",
-    description: "broken clouds",
-    icon: "04n"
-}
-
-var Main = {
-    temp: 289.5,
-    humidity: 89,
-    pressure: 1013,
-    temp_min: 287.04,
-    temp_max: 292.04
-}
-
-var Wind = {
-    speed: 5.1,
-    deg: 150
-}
-
-var Rain = {
-    "3h": 0
-}
-
-var Clouds = {
-    all: 75
-}
-
-var WeatherMain = {
-    coordinate: Coordinate,
-    sys: Sys,
-    weather: Weather,
-    main: Main,
-    wind: Wind,
-    rain: Rain,
-    clouds: Clouds,
-    dt: 1369824698,
-    id: 1851632,
-    name: "Milano",
-    base: "cmc stations",
-    cod: 200
-}
-
