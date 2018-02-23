1e9ae4bd7cfa3c94d6bfec8b439a063017c6bbc4
Amila Fernando <amila_fernandp92@gmail.com>
2018-02-23 15:01:29 +0100

    modifiche

--- JavaScript/jsfunctions.js
+++ JavaScript/jsfunctions.js
@@ -2,37 +2,13 @@
  * global variable
  */
 var map, infoWindow, globlalweather, globalperson;
+var pos = {
+    lat: 45.5094965,
+    lng: 9.2315997
+};
 
 $(document).ready(function(){
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {
-                lat: 45.5094965,
-                lng: 9.2315997
-            },
-            zoom: 14
-        });
-        infoWindow = new google.maps.InfoWindow;
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-                infoWindow.setPosition(pos);
-                infoWindow.setContent('You are Here!!');
-                infoWindow.open(map);
-                map.setCenter(pos);
-            }, function() {
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
-    }
-    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.lat + "&lon=" + pos.lng + "&APPID=ee6b293d773f4fcd7e434f79bbc341f2"
+    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.lat + "&lon=" + pos.lng + "&APPID=ee6b293d773f4fcd7e434f79bbc341f2";
     $.getJSON(url, function(data) {
         globlalweather = data;
     });
@@ -43,17 +19,43 @@ $(document).ready(function(){
     addTable ();
 });
 
-function caricaDati () {
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: pos.lat,
+            lng: pos.lng
+        },
+        zoom: 14
+    });
+    infoWindow = new google.maps.InfoWindow;
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            pos.lat = position.coords.latitude;
+            pos.lng = position.coords.longitude;
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You are Here!!');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 
 function addName () {
-    var person = globalperson.results[0];
+    $("#nome").text("amila fernando");
+    $("#data").text("AMILA FERNANDO");
+    /*var person = globalperson.results[0];
     if (person.gender=="male") {
         $("nome").text(person.name.first + " " + person.name.last);
     } else {
         $("nome").text(person.name.first + " " + person.name.last);
     }
-    $("data").text(person.registered);
+    $("data").text(person.registered);*/
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -62,12 +64,11 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
 }
 
-function addTable (arr) {
-    var weather = globlalweather.results[0];
-    $("idwind").text(weather.wind.speed);
-    $("iddescription").text(weather.weather["description"]);
-    $("idpressure").text(weather.main.pressure);
-    $("idtemperature").text(weather.main.temp);
-    $("idsunset").text(weather.sys.sunset);
-    $("idsunrise").text(weather.sys.sunrise);
+function addTable () {
+    $("#wind").text(globlalweather.wind.speed);
+    $("#description").text(globlalweather.weather[2].description);
+    $("#pressure").text(globlalweather.main.pressure);
+    $("#temperature").text(globlalweather.main.temp);
+    $("#sunset").text(globlalweather.sys.sunset);
+    $("#sunrise").text(globlalweather.sys.sunrise);
 }
