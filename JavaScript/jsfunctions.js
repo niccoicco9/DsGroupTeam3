218aa4ca22a7740965110f448ff48dbcaaa3343d
Amila Fernando <amila_fernandp92@gmail.com>
2018-02-23 14:43:30 +0100

    modifiche

--- JavaScript/jsfunctions.js
+++ JavaScript/jsfunctions.js
@@ -4,69 +4,69 @@
 var map, infoWindow, globlalweather, globalperson;
 
 $(document).ready(function(){
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=ee6b293d773f4fcd7e434f79bbc341f2", function(data) {
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                lat: 45.5094965,
+                lng: 9.2315997
+            },
+            zoom: 14
+        });
+        infoWindow = new google.maps.InfoWindow;
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('You are Here!!');
+                infoWindow.open(map);
+                map.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    }
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + pos.lat + "&lon=" + pos.lng + "&APPID=ee6b293d773f4fcd7e434f79bbc341f2", function(data) {
         globlalweather = data;
     });
     $.getJSON("https://randomuser.me/api/?results=1", function(data) {
         globalperson = data;
     });
+    addName ();
     addTable ();
 });
 
+function caricaDati () {
+}
 
-function initMap () {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 45.5094965,
-            lng: 9.2315997
-        },
-        zoom: 14
-    });
-    infoWindow = new google.maps.InfoWindow;
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('You are here');
-            infoWindow.open(map);
-            map.setCenter(pos);
-        },functionError);
+function addName () {
+    var person = globalperson.results[0];
+    if (person.gender=="male") {
+        $("nome").text(person.name.first + " " + person.name.last);
     } else {
-        functionError
+        $("nome").text(person.name.first + " " + person.name.last);
     }
+    $("data").text(person.registered);
 }
 
-function caricaDati (nome, data, titolo) {
-    addName (nome, data, titolo);
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition (functionGo, functionError);
-    } else {
-        functionError
-    }
-}
-
-function addName (nome, data) {
-    document.getElementById ("nome").innerText = nome;
-    document.getElementById ("data").innerText = data;
-}
-
-function functionGo (posizione){
-    document.getElementById ("idgeocoor").innerText = "[" + posizione.coords.latitude.toFixed (2) + "," + posizione.coords.longitude.toFixed (2) + "]";
-}
-
-function functionError (error) {
-    document.getElementById ("idgeocoor").innerText = error.message;
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
 
 function addTable (arr) {
     var weather = globlalweather.results[0];
-    document.getElementById ("idwind").innerText = weather.wind.speed;
-    document.getElementById ("iddescription").innerText = weather.weather["description"];
-    document.getElementById ("idpressure").innerText = weather.main.pressure;
-    document.getElementById ("idtemperature").innerText = weather.main.temp;
-    document.getElementById ("idsunset").innerText = weather.sys.sunset;
-    document.getElementById ("idsunrise").innerText = weather.sys.sunrise;
+    $("idwind").text(weather.wind.speed);
+    $("iddescription").text(weather.weather["description"]);
+    $("idpressure").text(weather.main.pressure);
+    $("idtemperature").text(weather.main.temp);
+    $("idsunset").text(weather.sys.sunset);
+    $("idsunrise").text(weather.sys.sunrise);
 }
