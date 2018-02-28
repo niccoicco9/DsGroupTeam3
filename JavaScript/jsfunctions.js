a8054554be84af09822cc6ece6cef70f93c801cb
Amila Fernando <amila_fernandp92@gmail.com>
2018-02-28 13:35:40 +0100

    modifiche vari

--- JavaScript/jsfunctions.js
+++ JavaScript/jsfunctions.js
@@ -17,6 +17,13 @@ $(document).ready(function(){
             functionGo ();
         });
     }, 10000);
+    setTimeout( function() {
+        var url = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + pos.lat + "&lon=" + pos.lng + "&APPID=ee6b293d773f4fcd7e434f79bbc341f2&";
+        $.getJSON(url, function(dataf) {
+            $(document).delay(2000);
+            addTableForecast (dataf);
+        });
+    }, 10000);
     $.getJSON("https://randomuser.me/api/?results=1", function(datap) {
         addName (datap);
     });
@@ -75,7 +82,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
 }
 function addTable (app) {
-    $("#wind").text(app.wind.speed + " m/s" + " " + windDirection(app.wind.deg));
+    $("#wind").text(app.wind.speed + " m/s" + "\n" + windDirection(app.wind.deg));
     $("#description").text(app.weather[0].description);
     $("#pressure").text(app.main.pressure + " hpa");
     $("#humidity").text(app.main.humidity + "%");
@@ -83,6 +90,31 @@ function addTable (app) {
     $("#sunset").text((new Date(app.sys.sunset*1000)).toLocaleTimeString());
     $("#sunrise").text((new Date(app.sys.sunrise*1000)).toLocaleTimeString());
     $("#temperature").text((app.main.temp-274.15).toFixed (0) + "°C");
+}
+function addTableForecast (app) {
+    console.log (app); 
+    for (var cont=1;cont<7;cont++) {
+        $("#day"+cont).text(getNextDay(app.list[cont].dt*1000));
+        $("#mintemperature"+cont).text(app.list[cont].temp.min);
+        $("#maxtemperature"+cont).text(app.list[cont].temp.max);
+        $("#daytemperature"+cont).text(app.list[cont].temp.day);
+        $("#morningtemperature"+cont).text(app.list[cont].temp.morn);
+        $("#eveningtemperature"+cont).text(app.list[cont].temp.eve);
+        $("#nighttemperature"+cont).text(app.list[cont].temp.night);
+        $("#pressure"+cont).text(app.list[cont].pressure);
+        $("#cloudness"+cont).text(app.list[cont].clouds);
+        $("#humidity"+cont).text(app.list[cont].humidity);
+        $("#icon"+cont).text(app.list[cont].dt);
+        $("#weather"+cont).text(app.list[cont].weather[0].main);
+        $("#weathercondition"+cont).text(app.list[cont].weather[0].description);
+        $("#windspeed"+cont).text(app.list[cont].speed);
+        $("#windirection"+cont).text(app.list[cont].deg);
+    }
+}
+function getNextDay (app) {
+   var date = new Date(app);
+   var formatted = date.format('ddd DD/MM/YYYY');
+   return formatted;
 }
 function windDirection (deg) {
     var north = "North";
@@ -93,48 +125,48 @@ function windDirection (deg) {
         return north+"("+deg+")";
     }
     if (deg>11.25 && deg<33.75) {
-        return north+" "+north+" "+est+"("+deg+")";
+        return north+"-"+north+"-"+est+"("+deg+")";
     }
     if (deg>33.75 && deg<56.25) {
-        return north+" "+est+"("+deg+")";
+        return north+"-"+est+"("+deg+")";
     }
     if (deg>56.25 && deg<78.75) {
-        return est+" "+north+" "+est+"("+deg+")";
+        return est+"-"+north+"-"+est+"("+deg+")";
     }
     if (deg>78.75 && deg<101.25) {
         return est+"("+deg+")";
     }
     if (deg>101.25 && deg<123.75) {
-        return est+" "+south+" "+est+"("+deg+")";
+        return est+"-"+south+"-"+est+"("+deg+")";
     }
     if (deg>123.75 && deg<146.25) {
-        return south+" "+est+"("+deg+")";
+        return south+"-"+est+"("+deg+")";
     }
     if (deg>146.25 && deg<168.75) {
-        return south+" "+south+" "+est+"("+deg+")";
+        return south+"-"+south+"-"+est+"("+deg+")";
     }
     if (deg>168.75 && deg<191.25) {
         return south+"("+deg+")";
     }
     if (deg>191.25 && deg<213.75) {
-        return south+" "+south+" "+west+"("+deg+")";
+        return south+"-"+south+"-"+west+"("+deg+")";
     }
     if (deg>213.75 && deg<236.25) {
-        return south+" "+west+"("+deg+")";
+        return south+"-"+west+"("+deg+")";
     }
     if (deg>236.25 && deg<258.75) {
-        return west+" "+south+" "+west+"("+deg+")";
+        return west+"-"+south+"-"+west+"("+deg+")";
     }
     if (deg>258.75 && deg<281.25) {
         return west+"("+deg+")";
     }
     if (deg>281.25 && deg<303.75) {
-        return west+" "+north+" "+west+"("+deg+")";
+        return west+"-"+north+"-"+west+"("+deg+")";
     }
     if (deg>303.75 && deg<326.25) {
-        return north+" "+west+"("+deg+")";
+        return north+"-"+west+"("+deg+")";
     }
     if (deg>326.25 && deg<348.75) {
-        return north+" "+north+" "+west+"("+deg+")";
+        return north+"-"+north+"-"+west+"("+deg+")";
     }
 }
